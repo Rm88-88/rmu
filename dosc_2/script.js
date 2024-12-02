3e802959b46b79daf80e1044e280ea497bc82348
Rm88-88 <rwbn83137@gmail.com>
2024-12-02 12:40:31 +0200

    Update and rename script.js to dosc_2/script.js

---
+++ dosc_2/script.js
@@ -0,0 +1,12 @@
+```javascript
+function openFolder(folderName) {
+    alert('פתחת תיקיה: ' + folderName);
+}
+
+function uploadImage() {
+    alert('תמונה הועלתה!');
+}
+
+function checkImageQuality() {
+    document.getElementById('quality-result').innerText = 'בדיקת איכות התמונה מתבצעת...';
+}
